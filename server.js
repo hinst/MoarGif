9495f646c35a8b89c4931e1a1387b15032c02a99
hinst <hinstance@yandex.ru>
2016-09-28 13:34:56 +0300

    Added UI for file uploading

--- server.js
+++ server.js
@@ -7,53 +7,66 @@ const portNumber = 9001;
 const latinAlphabetLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 app.listen(portNumber);
 const loadLayoutString = function() {
-    return fileSystem.readFileSync(__dirname + "/page/layout.html", "utf8");
+	return fileSystem.readFileSync(__dirname + "/page/layout.html", "utf8");
+};
+String.prototype.replaceAll =  function(a, b) {
+	let text = this;
+	while (text.indexOf(a) != -1) {
+		text = text.replace(a, b);
+	}
+	return text;
 };
 const checkPageNameRestriction = function(pageName) {
-    let result = pageName.length > 0;
-    if (result) {
-        for (let i = 0; i < pageName.length; i++) {
-            result = latinAlphabetLetters.indexOf(pageName[i]) > -1;
-            if (!result)
-                break;
-        }
-    }
-    return result;
+	let result = pageName.length > 0;
+	if (result) {
+		for (let i = 0; i < pageName.length; i++) {
+			result = latinAlphabetLetters.indexOf(pageName[i]) > -1;
+			if (!result)
+				break;
+		}
+	}
+	return result;
 };
 const loadPageString = function(pageName) {
-    let content = null;
-    if (checkPageNameRestriction(pageName)) {
-        try {
-            content = fileSystem.readFileSync(__dirname + "/page/" + pageName + ".html");
-        } catch (err) {
-            if (err.code = "ENOENT") {
-                content = null;
-            } else {
-                throw err;
-            }
-        }
-    }
-    return content;
+	let content = null;
+	if (checkPageNameRestriction(pageName)) {
+		try {
+			content = fileSystem.readFileSync(__dirname + "/page/" + pageName + ".html");
+		} catch (err) {
+			if (err.code = "ENOENT") {
+				content = null;
+			} else {
+				throw err;
+			}
+		}
+	}
+	if (content != null)
+		content = content.toString();
+	return content;
 };
 const substituteTemplate = function(text) {
-    return text.replace("$appURL", appURL);
+	console.log(text);
+	return text.replaceAll("$appURL", appURL);
 }
 const loadPage = function(pageName) {
-    let result = null;
-    const layout = substituteTemplate(loadLayoutString());
-    const content = loadPageString(pageName);
-    if (content != null) {
-        result = layout.replace("$document", content);
-    }
-    return result;
+	let result = null;
+	const layout = substituteTemplate(loadLayoutString());
+	let content = loadPageString(pageName);
+	if (content != null) {
+		content = substituteTemplate(content);
+		result = layout.replace("$document", content);
+	}
+	return result;
 };
 const pageHandler = function(request, response) {
-    let pageName = request.query.page;
-    if (pageName == undefined) {
-        pageName = "main";
-    }
-    const content = loadPage(pageName);
-    response.send(content);
+	let pageName = request.query.page;
+	if (pageName == undefined) {
+		pageName = "main";
+	}
+	const content = loadPage(pageName);
+	response.send(content);
 };
 app.get(appURL, pageHandler);
+app.use(appURL + "/third", express.static("third"));
+app.use(appURL + "/page", express.static("page"));
 console.log("app initialized");
